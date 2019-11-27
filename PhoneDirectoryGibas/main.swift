32643358077d8f40aa33f61a628ffe257c2591dc
gwojcik <zgagato@mac.com>
2019-11-27 20:48:45 +0100

    Initial Commit

---
+++ PhoneDirectoryGibas/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PhoneDirectoryGibas
+//
+//  Created by zgaga on 27-11-2019.
+//  Copyright © 2019 Hime. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
